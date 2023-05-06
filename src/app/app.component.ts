4ad6fc521369de5cb8e6c89b5b4ba02f453db86c
ufuk kuru <uege60@gmail.com>
2023-05-06 09:26:54 +0300

    angular reactive form sample 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reactive-form';
+export class AppComponent implements OnInit {
+ 
+  addForm:FormGroup = new FormGroup({})
+ 
+  ngOnInit(): void {
+    this.createAddForm()
+  }
+  
+  createAddForm(){
+   this.addForm = new FormGroup({
+     email: new FormControl("",[Validators.required,Validators.email]),
+     password: new FormControl("",[Validators.required,Validators.minLength(2),Validators.maxLength(10)])
+    })
+  }
+
+  getAdd(){
+    if(this.addForm.valid){
+      console.log(this.addForm.value)
+      this.addForm.reset()
+    }
+  }
 }
